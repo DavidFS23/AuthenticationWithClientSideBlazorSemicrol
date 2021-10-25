f55857eaca1fba749f789ce3ca37083c0a52f017
DavidFS23 <david23fs@gmail.com>
2021-10-25 10:07:53 +0200

    First upload.

---
+++ src/AuthenticationWithClientSideBlazor.Server/Controllers/AccountsController.cs
@@ -0,0 +1,21 @@
+﻿using AuthenticationWithClientSideBlazor.Shared;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationWithClientSideBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountsController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+    }
+}
